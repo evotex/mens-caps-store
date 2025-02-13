1ca0a4f146b82c1c71d0bb6c9f369f0b82621da7
Evotex <evotex@example.com>
2025-02-13 20:18:07 +0200

    image and pages

---
+++ pages/return.js
@@ -0,0 +1,5 @@
+import Page from "../components/Page";
+
+export default function ReturnPolicy() {
+  return <Page title="Политика возврата" content="Вы можете вернуть товар в течение 14 дней после покупки." />;
+}
---
+++ pages/shipping.js
@@ -0,0 +1,5 @@
+import Page from "../components/Page";
+
+export default function ShippingInfo() {
+  return <Page title="Информация о доставке" content="Доставка осуществляется по всей Украине в течение 2-5 рабочих дней." />;
+}
