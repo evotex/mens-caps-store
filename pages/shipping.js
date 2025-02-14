7a63bec44bbb7961234cd4058754526031491e5e
Evotex <evotex@example.com>
2025-02-14 14:11:20 +0200

    Исправлены пути к Page.js, проверка сборки

--- pages/shipping.js
+++ pages/shipping.js
@@ -1,4 +1,4 @@
-import Page from "@/components/Page";
+import Page from "../components/Page";
 
 export default function ShippingInfo() {
   return <Page title="Информация о доставке" content="Доставка осуществляется по всей Украине в течение 2-5 рабочих дней." />;
